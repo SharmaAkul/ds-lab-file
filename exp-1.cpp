e2271c5e12a31873dcff79f0fa972a9e02413db9
SharmaAkul <42888896+SharmaAkul@users.noreply.github.com>
2018-11-30 23:36:14 +0530

    Update exp-1.cpp

--- exp-1.cpp
+++ exp-1.cpp
@@ -1,8 +1,8 @@
 /* Q: Write a menu driven program that implements following operations on a  linear array:
-    -> Insert a new element at a specified position
-    -> Delete an element either whose value is given or whose position is given
-    -> To find the location of a given element
-    -> To display the elements of the linear array
+    a. Insert a new element at a specified position
+    b. Delete an element either whose value is given or whose position is given
+    c.To find the location of a given element
+    d. To display the elements of the linear array
 */
 
 #include<iostream>
